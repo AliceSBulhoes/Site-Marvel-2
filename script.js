226e5aa545d4ef06258a95f962410a3f1b056955
Alice <alicesb2006@gmail.com>
2024-05-14 09:45:56 -0300

    Terminando a função contador()

--- script.js
+++ script.js
@@ -15,5 +15,39 @@ function slideShow(){
 slideShow();
 
 function contador(){
-    
+    const contador = document.getElementById("contador-1");
+    let valor = 0;
+    let tempo = setInterval(()=>{
+        valor += 1;
+
+        contador.innerHTML = ` + ${valor}`
+
+        if(valor == 300){
+            clearInterval(tempo)
+        }
+    });
+
+    const contador2 = document.getElementById("contador-2");
+    let valor2 = 0;
+    let tempo2 = setInterval(()=>{
+        valor2 += 1;
+
+        contador2.innerHTML = ` + ${valor2}`
+
+        if(valor2 == 500){
+            clearInterval(tempo2)
+        }
+    });
+
+    const contador3 = document.getElementById("contador-3");
+    let valor3 = 0;
+    let tempo3 = setInterval(()=>{
+        valor3 += 1;
+
+        contador3.innerHTML = ` + ${valor3}`;
+
+        if(valor3 == 1000){
+            clearInterval(tempo3);
+        }
+    });
 }
