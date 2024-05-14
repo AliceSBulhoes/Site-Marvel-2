26b87d9eac4660deba268ffd1804002a6c12b4eb
Alice <alicesb2006@gmail.com>
2024-05-14 09:30:52 -0300

    Criando a parte do banner

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let banners = ["./img/banner1.png","./img/banner2.png", "./img/banner3.png"]
+let index = 0;
+
+function slideShow(){
+    let banner = document.getElementById("banners").src = banners[index];
+    index ++
+
+    if(index == 3){
+        index = 0;
+    }
+    setTimeout('slideShow()',2000)
+    
+}
+slideShow();
